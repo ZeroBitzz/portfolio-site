1c091a3e1b40d167debffec133ebcb62712922a6
Broderick <zerothesu@gmail.com>
2024-04-29 21:45:47 -0600

    removed semicolons from js file

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,10 +1,10 @@
 // removes the loading page
 setTimeout(() => {
     document.getElementById("loader-wrapper").style.display = "none"
-}, 3000);
+}, 3000)
 
 // clock for the website
 setInterval(() => {
-    const timeDisplay = document.getElementById("time");
-    timeDisplay.textContent = new Date().toLocaleString().replace(", ", " - ");
-}, 1000);
+    const timeDisplay = document.getElementById("time")
+    timeDisplay.textContent = new Date().toLocaleString().replace(", ", " - ")
+}, 1000)
